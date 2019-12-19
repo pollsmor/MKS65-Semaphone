b2176df411f451550752a9b531cff7293efc301b
Kevin Li <kevinli4321@gmail.com>
2019-12-19 18:26:34 -0500

    -v arg for control now works

--- control.c
+++ control.c
@@ -2,44 +2,55 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define KEY 24602
-
-void create_sem(int *semd);
+#define SEMKEY 24602
+#define SHMKEY 24603
+#define SEG_SIZE 1000
 
 int main(int argc, char *argv[]) {
   if (argc > 1) {
     int semd;
+    int shmd;
+    int fd;
 
     if (strcmp(argv[1], "-c") == 0) {
-      create_sem(&semd);
-      return 0;
-    }
+      semd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+      printf("semaphore created \n");
 
-    else if (strcmp(argv[1], "-r") == 0) {
+      shmd = shmget(SHMKEY, SEG_SIZE, IPC_CREAT | IPC_EXCL | 0644);
+      printf("shared memory created \n");
+
+      fd = open("story.txt", O_CREAT | O_TRUNC, 0644);
+      printf("file created \n");
+
       return 0;
     }
 
     else if (strcmp(argv[1], "-v") == 0) {
+      fd = open("story.txt", O_RDONLY);
+      char story[SEG_SIZE];
+      read(fd, story, SEG_SIZE);
+      printf("%s", story);
+      close(fd);
+
+      return 0;
+    }
+
+    else if (strcmp(argv[1], "-r") == 0) {
+
+
       return 0;
     }
 
     else {
-      printf("Invalid arguments. Only -c, -r, and -v are supported. \n");
+      printf("Invalid arguments. Only -c, -v, and -r are supported. \n");
       return 1;
     }
   }
 
-  printf("You need to provide either the -c, -r, or -v argument to this program. \n");
+  printf("You need to provide either the -c, -v, or -r argument to this program. \n");
   return 1;
-}
-
-void create_sem(int *semd) {
-  *semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-  if (*semd == -1) {
-    printf("error %d: %s \n", errno, strerror(errno));
-    *semd = semget(KEY, 1, 0);
-  }
-
-  printf("semaphore created \n");
 }
